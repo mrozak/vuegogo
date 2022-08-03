34d2f95518057773afd22041106fea242b044427
muhrozak <rozak.tkj@gmail.com>
2022-08-03 14:08:49 +0700

    membuat komponen profile

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ProfileView">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <div class="media text-left">
+                    <img src="../assets/9.jpg" class="user-pic mr-3" />
+                    <div class="media-body">
+                        <h5 class="mt-0">Muhamad Rozak</h5>
+                        <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "ProfileView",
+  components: {}
+};
+</script>
+
+<style scoped>
+.user-pic {
+    border-radius: 200%;
+    height: 80px;
+    width: 80px;
+}
+
+</style>
